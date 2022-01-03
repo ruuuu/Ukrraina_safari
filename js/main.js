86d0ccc3bb822758bc1e6bd3ddb1bd704943e5a3
ruuuu <rufinka_91@mail.ru>
2022-01-03 20:32:54 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-$(function(){
-  $('.popup__link').magnificPopup({
+$(function () {
+	$('.popup__link').magnificPopup({
 		disableOn: 700,
 		type: 'iframe',
 		mainClass: 'mfp-fade',
@@ -9,13 +9,13 @@ $(function(){
 		fixedContentPos: false
 	});
 
-  $('.gallery__slider').slick({
-    prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="images/arrow-left.svg" alt=""></button>',
-    nextArrow: '<button type="button" class="slick-btn slick-next"><img src="images/arrow-right.svg" alt=""></button>'
-  }); // для слайдера https://kenwheeler.github.io/slick/
+	$('.gallery__slider').slick({
+		prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="images/arrow-left.svg" alt=""></button>',
+		nextArrow: '<button type="button" class="slick-btn slick-next"><img src="images/arrow-right.svg" alt=""></button>'
+	}); // для слайдера https://kenwheeler.github.io/slick/
 
 
-  $('.gallery__item-inner').magnificPopup({
+	$('.gallery__item-inner').magnificPopup({
 		delegate: 'a',
 		type: 'image',
 		tLoading: 'Loading image #%curr%...',
@@ -23,8 +23,22 @@ $(function(){
 		gallery: {
 			enabled: true,
 			navigateByImgClick: true,
-			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+			preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
 		}
 	});
 
+	$('.gallery__item-inner').magnificPopup({ // взяла отсюда  https://dimsemenov.com/plugins/magnific-popup/ жмем на lightBox Gallery
+		delegate: 'a',
+		type: 'image',
+		tLoading: 'Loading image #%curr%...',
+		mainClass: 'mfp-img-mobile',
+		gallery: {
+			enabled: true,
+			navigateByImgClick: true,
+			preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
+		},
+	});
+
 });
+
+
